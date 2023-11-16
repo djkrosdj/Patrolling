29c7d72b421313237303eee26fcd6346088d3032
Emil <54106517+djkrosdj@users.noreply.github.com>
2023-11-16 04:34:31 +0300

    fix: add comments in scripts

--- Assets/Scripts/PlayerPatrolling.cs
+++ Assets/Scripts/PlayerPatrolling.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Класс потрулирования объекта по точкам
+/// </summary>
 public class PlayerPatrolling : MonoBehaviour
 {
+    // Время ожидания между движениями к следующей точке
     [SerializeField] private float _whaitTime = 3f;
 
+    // Скорость движения персонажа
     [SerializeField] private float _speed = 2f;
 
+    // Индекс текущей точки, к которой движется персонаж
     private int _index;
+
+    // Текущее время ожидания
     private float _currentTime;
 
+    // Массив созданных точек для патрулирования
     private GameObject[] _spawnedPoints;
+
+    // Точка, к которой движется персонаж
     private Transform _destination;
 
-    // Start is called before the first frame update
+    // Вызывается при старте
     private void Start()
     {
         // Находим объект, содержащий PointsSpawn скрипт
@@ -30,35 +41,53 @@ public class PlayerPatrolling : MonoBehaviour
         Patrolling();
     }
 
-    // Update is called once per frame
+    // Вызывается каждый кадр
     private void Update()
     {
+        // Если еще не прошло нужное время ожидания
         if (_currentTime < _whaitTime)
         {
+            // Увеличиваем текущее время ожидания
             _currentTime += Time.deltaTime;
-            return;
+            return; // Прерываем выполнение метода Update
         }
 
+        // Вычисляем расстояние, которое нужно пройти за текущий кадр
         var travelDistance = _speed * Time.deltaTime;
+
+        // Поворачиваем персонажа в сторону точки, к которой движемся
         transform.LookAt(_destination);
+
+        // Вычисляем новую позицию персонажа
         var newPosition = Vector3.MoveTowards(transform.position, _destination.position, travelDistance);
+
+        // Устанавливаем новую позицию персонажа
         transform.position = newPosition;
 
+        // Если персонаж достиг точки назначения
         if (Vector3.Distance(transform.position, _destination.position) < 0.5)
         {
+            // Увеличиваем индекс для движения к следующей точке
             _index++;
+
+            // Вызываем метод патрулирования для движения к следующей точке
             Patrolling();
+
+            // Сбрасываем текущее время ожидания
             _currentTime = 0f;
         }
     }
 
+    // Метод для выбора точки назначения для патрулирования
     private void Patrolling()
     {
-        if (_index > _spawnedPoints.Length-1)
+        // Если индекс больше, чем количество точек, сбрасываем его
+        if (_index > _spawnedPoints.Length - 1)
         {
             _index = 0;
         }
 
+        // Устанавливаем точку назначения
         _destination = _spawnedPoints[_index].transform;
     }
 }
--- Assets/Scripts/PointsSpawn.cs
+++ Assets/Scripts/PointsSpawn.cs
@@ -4,33 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+/// <summary>
+/// Класс спавна контрольных точек
+/// </summary>
 public class PointsSpawn : MonoBehaviour
 {
+    // Ссылки на места, где будут появляться объекты
     [SerializeField] private Transform[] _spawn;
 
+    // Префабы объектов, которые будут создаваться
     [SerializeField] private GameObject[] _pointPrefab;
 
+    // Массив для хранения созданных объектов
     private GameObject[] _spawnedPoints;
+
+    // Вызывается при запуске сцены перед любыми вызовами метода Start
     private void Awake()
     {
-        //Invoke("Spawn", 0.5f);
+        // Вызывает метод Spawn для создания объектов при запуске сцены
         Spawn();
     }
 
+    // Метод для создания и размещения объектов в указанных местах
     private void Spawn()
     {
+        // Инициализация массива для хранения созданных объектов
         _spawnedPoints = new GameObject[_spawn.Length];
-        
+
+        // Цикл для создания объектов для каждого указанного места
         for (var i = 0; i < _spawn.Length; i++)
         {
+            // Создание объекта из префаба
             var point = Instantiate(_pointPrefab[i]);
+
+            // Установка позиции созданного объекта в соответствии с указанным местом
             point.transform.position = _spawn[i].transform.position;
-            
+
+            // Добавление созданного объекта в массив для последующего доступа
             _spawnedPoints[i] = point;
         }
     }
-    
-    // Публичный метод для доступа к созданным объектам
+
+    // Публичный метод для доступа к созданным объектам из других скриптов
     public GameObject[] GetSpawnedPoints()
     {
         return _spawnedPoints;
